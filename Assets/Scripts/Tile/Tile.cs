4f4e7d8da309f9c3e0955806de55bc54c1652fd9
= <=>
2017-10-15 14:05:18 -0300

    map construction

---
+++ Assets/Scripts/Tile/Tile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Tile : MonoBehaviour 
+{
+    private void OnDrawGizmos()
+    {
+        SpriteRenderer[] renderer = GetComponentsInChildren<SpriteRenderer>();
+
+        if (renderer != null)
+        {
+            for (int i = 0; i < renderer.Length; i++)
+            {
+                renderer[i].sortingOrder = (int)(transform.position.y * 10 * -1);
+            }
+        }
+    }
+}
